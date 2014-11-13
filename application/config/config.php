a88b30a3c77f6d122484ad4e0a791c4a8987ee49
CongyuanTang <tangcongyuan@foxmail.com>
2014-11-13 15:33:18 -0500

    Fix the error and scaffolding is good to go.

--- application/config/config.php
+++ application/config/config.php
@@ -53,7 +53,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/rms';
+$config['base_url'] = 'http://localhost/WPI_CS542_rms';
 
 /*
 |--------------------------------------------------------------------------
